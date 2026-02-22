32b5fa40a1b559c1571135887d2bddf580ee3792
himayeah <nirmanihimaya@gmail.com>
2026-02-22 17:47:24 +0530

    Invoice Module- Popup added

--- frontend/src/app/demo/components/pages/pages.module.ts
+++ frontend/src/app/demo/components/pages/pages.module.ts
@@ -13,6 +13,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 import { ClientRegComponent } from './client-reg/client-reg.component';
 import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
 import { AppointmentScheduleComponent } from './appointment-schedule/appointment-schedule.component';
@@ -33,6 +34,8 @@ import { SupplierComponent } from './supplier/supplier.component';
 import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
 import { PurchaseOrderDetailComponent } from './purchase-order-detail/purchase-order-detail.component';
 import { InvoiceComponent } from './invoice/invoice.component';
+import { InvoiceItemModalComponent } from './invoice/invoice-item-modal/invoice-item-modal.component';
+import { InvoicePaymentModalComponent } from './invoice/invoice-payment-modal/invoice-payment-modal.component';
 
 
 @NgModule({
@@ -53,7 +56,9 @@ import { InvoiceComponent } from './invoice/invoice.component';
     SupplierComponent,
     PurchaseOrderComponent,
     PurchaseOrderDetailComponent,
-    InvoiceComponent
+    InvoiceComponent,
+    InvoiceItemModalComponent,
+    InvoicePaymentModalComponent
   ],
 
   imports: [
@@ -74,6 +79,7 @@ import { InvoiceComponent } from './invoice/invoice.component';
     MatSelectModule,
     MatSlideToggleModule,
     MatCheckboxModule,
+    MatDialogModule,
   ]
 })
 export class PagesModule { }
