af0627659097f378babd5e9bccdb0754a00ad155
himayeah <nirmanihimaya@gmail.com>
2026-03-28 23:53:26 +0530

    commit 1

--- backend/src/main/java/com/bit/backend/repositories/AppointmentScheduleRepository.java
+++ backend/src/main/java/com/bit/backend/repositories/AppointmentScheduleRepository.java
@@ -4,7 +4,6 @@ import com.bit.backend.entities.AppointmentScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 public interface AppointmentScheduleRepository extends JpaRepository<AppointmentScheduleEntity, Long> {
 
     // Dashboard Card (Total Appointments in last 30 Days)
@@ -13,8 +12,10 @@ public interface AppointmentScheduleRepository extends JpaRepository<Appointment
 
     @Query(value = "SELECT s.service_name " +
             "FROM appointment_schedule a " +
-            "JOIN service s ON s.id = a.service_id WHERE STR_TO_DATE(a.appointment_date, '%Y-%m-%d') BETWEEN CURRENT_DATE - INTERVAL 30 DAY AND CURRENT_DATE " +
+            "JOIN service s ON s.id = a.service_id WHERE STR_TO_DATE(a.appointment_date, '%Y-%m-%d') BETWEEN CURRENT_DATE - INTERVAL 30 DAY AND CURRENT_DATE "
+            +
             "GROUP BY s.service_name " +
             "ORDER BY COUNT(*) DESC LIMIT 1", nativeQuery = true)
     String getMostUsedService();
+
 }
