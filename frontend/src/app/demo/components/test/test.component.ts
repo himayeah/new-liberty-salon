2d77f0e238fad4795672128b81235503bb3713cd
himayeah <nirmanihimaya@gmail.com>
2026-04-05 11:22:16 +0530

    fix: corrected gitignore

--- frontend/src/app/demo/components/test/test.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test',
-  standalone: false,
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.scss',
-})
-export class TestComponent {}
--- frontend/src/app/form-demo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormDemoDirective } from './form-demo.directive';
-
-describe('FormDemoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FormDemoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
